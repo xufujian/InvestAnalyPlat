8d922cc4d2fcca6b164b1329f204becace5e2104
xfjjs@live.com <xfjjs@live.com>
2022-08-16 23:19:49 +0800

    完成诸葛所在城市下列表页的开发

--- sector/src/main/java/com/plate/house/HouseSpiderJob.java
+++ sector/src/main/java/com/plate/house/HouseSpiderJob.java
@@ -10,6 +10,7 @@ import us.codecraft.webmagic.processor.example.GithubRepoPageProcessor;
 import us.codecraft.webmagic.selector.Selectable;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author: 今天风很大
@@ -25,22 +26,48 @@ public class HouseSpiderJob implements PageProcessor {
             .setSleepTime(1000)
             .setTimeOut(10000)
             .setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0")
-            .addCookie("acw_sc__v2", "62fa6274682c4e9ab139b9ddff165fe445606df3");
+            .addCookie("acw_sc__v2", "62fbafa54590788d0dad578edef75ab238b50079");
 
     @Override
     public void process(Page page) {
         System.out.println("in page...");
         //房屋总量
         String houseTotalNum = page.getHtml().xpath("//div[@class='list-converge-box']/strong/text()").get();
-        List<Selectable> nodesList = page.getHtml().xpath("//div[@class='exponent-div']").nodes();
+        List<Selectable> nodesAnsList = page.getHtml().xpath("//div[@class='exponent-div']").nodes();
         //城市名称
-        String cityName = nodesList.get(0).xpath("//h5/text()").toString();
-        String cityAvgPrice = nodesList.get(1).xpath("//p[@class=num]/text()").get();
-        String lastMonth = nodesList.get(2).xpath("//p[@class=num]/text()").get();
-        String markDownPriceNum = nodesList.get(3).xpath("//p[@class=num]/a/text()").get();
-        String newHouseNum = nodesList.get(4).xpath("//p[@class=num]/a/text()").get();
+        String cityName = nodesAnsList.get(0).xpath("//h5/text()").toString();
+        String cityAvgPrice = nodesAnsList.get(1).xpath("//p[@class=num]/text()").get();
+        String lastMonth = nodesAnsList.get(2).xpath("//p[@class=num]/text()").get();
+        String markDownPriceNum = nodesAnsList.get(3).xpath("//p[@class=num]/a/text()").get();
+        String newHouseNum = nodesAnsList.get(4).xpath("//p[@class=num]/a/text()").get();
 
-        System.out.println("houseTotalNum:" +  houseTotalNum+" cityName:"+cityName+": cityAvgPrice:"+cityAvgPrice+" lastMonth:"+lastMonth+ " markDownPriceNum: "+markDownPriceNum+" newHouseNum: "+newHouseNum);
+        System.out.println("houseTotalNum:" + houseTotalNum + " cityName:" + cityName + ": cityAvgPrice:" + cityAvgPrice + " lastMonth:" + lastMonth + " markDownPriceNum: " + markDownPriceNum + " newHouseNum: " + newHouseNum);
+
+        List<Selectable> nodesList = page.getHtml().xpath("//ul[@class='list-table-box']/li").nodes();
+        for (int i = 0; i < nodesList.size(); i++) {
+            Selectable sele = nodesList.get(i);
+            //房源标题
+            String title = sele.xpath("//a[@class='list-table-tab-box']/@alt").get();
+            //房源地址
+            String href = sele.xpath("//a[@class='list-table-tab-box']/@href").get();
+            Selectable spanList = sele.xpath("//span[@class='list-table-info-box']");
+            //房源基本信息
+            String propertyBaseInfo = spanList.xpath("//span[2]/text()").get();
+            List<String> addrList = spanList.xpath("//span[1]//a[@class='area_class']/text()").all();
+            //房源地址
+            String addr = addrList.stream().map(String::valueOf).collect(Collectors.joining(","));
+            //更新时间
+            String updateTime = spanList.xpath("//span[3]/em/text()").get();
+            //房源总价
+            String totalPrice = sele.xpath("//span[@class='right_span']/span[1]/b/text()").get();
+            //房源均价
+            String propertyAvgPrice = sele.xpath("//span[@class='right_span']/strong/text()").get();
+
+            System.out.println("title: "+title+" href:"+href +" propertyBaseInfo:"+propertyBaseInfo+" addr:"+addr+" updateTime" +updateTime +" totalPrice:"+totalPrice+" propertyAvgPrice:"+propertyAvgPrice);
+
+        }
+
+
 
     }
 
